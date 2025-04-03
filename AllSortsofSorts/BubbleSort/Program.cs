d3ecd238a8f3fa6fba1ad74bfc1cba0bdb34cb97
Aswin Francis <aswinrulez@gmail.com>
2025-04-03 16:19:47 +0530

    Initial commit with first project

---
+++ AllSortsofSorts/BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var arrayToSort = new int[] { 5, 8, 7, 5, 3 };
+var sortedArray = BubbleSort(arrayToSort);
+Console.WriteLine(string.Join(',', sortedArray.Select(x => x)));
+return;
+
+int[] BubbleSort(int[] inputArray)
+{
+    var hasSwapped = false;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        for (int j = 0; j < inputArray.Length - 1 - i; j++)
+        {
+            if (inputArray[j] > inputArray[j + 1])
+            {
+                //C#'s tuple deconstruction syntax, which was introduced in C# 7.0.
+                //Same as:
+                // var temp = inputArray[j];
+                // inputArray[j] = inputArray[j + 1];
+                // inputArray[j + 1] = temp;
+                (inputArray[j], inputArray[j + 1]) = (inputArray[j + 1], inputArray[j]);
+                hasSwapped = true;
+            }
+        }
+
+        if (!hasSwapped)
+            break;
+    }
+
+    return inputArray;
+}
