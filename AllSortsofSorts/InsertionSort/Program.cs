4c91c6af0b19a1ccde35bc7ba16adc6ed3bd836c
Aswin Francis <afrancis@gsiccorp.net>
2025-06-11 11:43:04 +0530

    Implement basic insertion sort algorithm in Program.cs

--- AllSortsofSorts/InsertionSort/Program.cs
+++ AllSortsofSorts/InsertionSort/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Insertion Sort");
+
+var arrayToSort = new int[] { 5, 8, 7, 5, 3 };
+var sortedArray = InsertionSort(arrayToSort);
+Console.WriteLine(string.Join(',', sortedArray.Select(x => x)));
+
+
+int[] InsertionSort(int[] inputArray)
+{
+    for (int i = 1; i < inputArray.Length; i++)
+    {
+        var j = i - 1;
+        var currentValue = inputArray[i];
+        while (j > -1 && inputArray[j] > currentValue)
+        {
+            inputArray[j + 1] = inputArray[j];
+            j--;
+        }
+        inputArray[j + 1] = currentValue;
+    }
+
+    return inputArray;
+}
